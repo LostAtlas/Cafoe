28f0d72fdd995967654eff335e38467b476d84d8
neonneom <deandreholmes55@gmail.com>
2018-12-26 13:18:00 -0500

    Added event listener to run ReactDom.hydrate only when the dom is ready

--- LostAtlasCAH.React/src/browser/client_index.js
+++ LostAtlasCAH.React/src/browser/client_index.js
@@ -3,8 +3,14 @@ import ReactDom from "react-dom";
 import App from '../shared/components/App'
 import {BrowserRouter} from "react-router-dom";
 
+document.addEventListener('readystatechange', event => {
+    if (event.target.readyState === 'complete') {
 
-ReactDom.hydrate(
-<BrowserRouter>
-    <App />
-</BrowserRouter>, document.getElementById('app'));
+        ReactDom.hydrate(
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>, document.getElementById('app'));
+
+    }
+  });
+

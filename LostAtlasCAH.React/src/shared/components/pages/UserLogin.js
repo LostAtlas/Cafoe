b1148252bb556295158f2d2c731a35522a997e5c
neonneom <deandreholmes55@gmail.com>
2018-12-27 04:21:18 -0500

    Added new font and more styling, also reorganized navigation

---
+++ LostAtlasCAH.React/src/shared/components/pages/UserLogin.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+class UserLogin extends Component {
+
+  render() {
+    return (
+      <div id="enterPage">
+        <div id="centerBox">
+                   <img src={require("../../images/logo1.png")} alt="Logo" />
+                   <h3>CARDS AGAINST HUMANITY ONLINE</h3>
+                   
+                   <div>
+                        <input type="text" name="username" placeholder="Username" />
+                        <p>#0000</p>
+                   </div>
+                   <NavLink exact to="/dashboard">
+                        <p id="submit">ENTER</p>
+                   </NavLink>
+                   
+        </div>
+      </div>
+    );
+  }
+}
+export default UserLogin;

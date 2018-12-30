4fbe4193266abd70b8f253a4f0c34529b0b8d6aa
neonneom <deandreholmes55@gmail.com>
2018-12-30 18:27:30 -0500

    Fixed signalr multiconnection per browser tab issue

--- LostAtlasCAH.API/LostAtlasCAH.API/Hub.cs
+++ LostAtlasCAH.API/LostAtlasCAH.API/Hub.cs
@@ -1,13 +1,39 @@
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
 
 namespace SignalRChat.Hubs
 {
+    public static class UserHandler
+    {
+        public static HashSet<string> ConnectedIds = new HashSet<string>();
+    }
     public class ChatHub : Hub
     {
+        public override async Task OnConnectedAsync()
+        {
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+            Console.WriteLine( "\n\n\n"  + UserHandler.ConnectedIds.Count + "\n\n\n");
+            await base.OnConnectedAsync();
+        }
+        public async Task JoinGameSession(string[] ids)
+        {
+            string gameId = ids[0];
+            string userId = ids[1];
+            await Clients.All.SendAsync("incomingMessage", "Joined Game Session: " + gameId + "\nUser ID: " + userId + "\nConnection ID: " + Context.ConnectionId);
+        }
+
         public async Task SendMessage(string message)
         {
-            await Clients.All.SendAsync("incomingMessage", message + " Bitch!!\nConnection ID: " + Context.ConnectionId);
+            await Clients.All.SendAsync("incomingMessage", message + " Bitch!!");
+        }
+        
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+            Console.WriteLine( "\n\n\n"  + UserHandler.ConnectedIds.Count + "\n\n\n");
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
--- LostAtlasCAH.React/src/shared/components/pages/About.js
+++ LostAtlasCAH.React/src/shared/components/pages/About.js
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
+const signalR = require("@aspnet/signalr");
+
+var signalrConnection = false;
+
 class About extends Component {
   render() {
+
+    var gameId= "123";
+    var userId = "963";
+    var SessionIds  = [gameId, userId];
+
+    let connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:5000/chatHub")
+        .build();
+    
+    
+    if(!signalrConnection){
+        connection.start()
+        .then(() => connection.invoke("joinGameSession", SessionIds))
+        .then(() => connection.invoke("sendMessage", "Hello"));
+
+        signalrConnection = true;
+    }
+
+    connection.on("incomingMessage", data => {
+        console.log(data);
+    });
+    
+
     return (
       <div className="Content">
         <h1>About</h1>

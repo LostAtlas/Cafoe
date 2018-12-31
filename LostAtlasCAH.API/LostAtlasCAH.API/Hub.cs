ff5815a17c58ef94a9dafd6f68a11ca92ac922b8
neonneom <deandreholmes55@gmail.com>
2018-12-30 19:43:53 -0500

    Added Single User Connections group & Game Instance group functionality

--- LostAtlasCAH.API/LostAtlasCAH.API/Hub.cs
+++ LostAtlasCAH.API/LostAtlasCAH.API/Hub.cs
@@ -7,20 +7,52 @@ namespace SignalRChat.Hubs
 {
     public static class UserHandler
     {
-        public static HashSet<string> ConnectedIds = new HashSet<string>();
+        //public static HashSet<string> ConnectedIds = new HashSet<string>();
+        public static Dictionary<string, List<string>> ConnectionGroups = new Dictionary<string, List<string>>();
+
     }
     public class ChatHub : Hub
     {
         public override async Task OnConnectedAsync()
         {
-            UserHandler.ConnectedIds.Add(Context.ConnectionId);
-            Console.WriteLine( "\n\n\n"  + UserHandler.ConnectedIds.Count + "\n\n\n");
+            //UserHandler.ConnectedIds.Add(Context.ConnectionId);
+            //Console.WriteLine( "\n\n\n"  + UserHandler.ConnectedIds.Count + "\n\n\n");
             await base.OnConnectedAsync();
         }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            //UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+            //Console.WriteLine( "\n\n\n"  + UserHandler.ConnectedIds.Count + "\n\n\n");
+
+            List<string> groups= new List<string>();
+            groups = UserHandler.ConnectionGroups[Context.ConnectionId];
+            
+            Console.WriteLine( "\n\n\n");
+            for (int i = 0; i < groups.Count; i++)
+            {
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, groups[i]);
+                Console.WriteLine( "Removed Connection: "  + Context.ConnectionId + " from group: " + groups[i]);
+            }
+            Console.WriteLine( "\n\n\n");
+            
+            UserHandler.ConnectionGroups.Remove(Context.ConnectionId);
+            await base.OnDisconnectedAsync(exception);
+        }
+
         public async Task JoinGameSession(string[] ids)
         {
             string gameId = ids[0];
             string userId = ids[1];
+            List<string> groups= new List<string>();
+            groups.Add(userId);
+            groups.Add(gameId);
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, userId);
+            await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
+            UserHandler.ConnectionGroups.Add(Context.ConnectionId,groups);
+            Console.WriteLine( "\n\n\nAdded Connection: "  + Context.ConnectionId + " to groups: " + gameId + " & " + userId + "\n\n\n");
+
             await Clients.All.SendAsync("incomingMessage", "Joined Game Session: " + gameId + "\nUser ID: " + userId + "\nConnection ID: " + Context.ConnectionId);
         }
 
@@ -29,11 +61,5 @@ namespace SignalRChat.Hubs
             await Clients.All.SendAsync("incomingMessage", message + " Bitch!!");
         }
         
-        public override async Task OnDisconnectedAsync(Exception exception)
-        {
-            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
-            Console.WriteLine( "\n\n\n"  + UserHandler.ConnectedIds.Count + "\n\n\n");
-            await base.OnDisconnectedAsync(exception);
-        }
     }
 }
